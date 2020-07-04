90a736f04be7cc779a97b5b44dd55012fde69eae
Shawn Han <tosky0514@gmail.com>
2020-07-04 15:44:10 +0900

    Completed 01_convertToString.js

---
+++ 01_convertToString.js
@@ -0,0 +1,7 @@
+//convertToString함수가 있습니다. 이 함수는 주어진 파라미터를 문자열의 형태로 변환합니다.
+
+//노트: 파라미터가 객체이거나, 배열일 경우는 신경쓰지 않아도 좋습니다
+
+function convertToString(anything) {
+    return anything.toString();    // 여기에 코드를 작성하세요
+}
