ec041110084c4c33391f0b6eaee0f0ee747132d6
Shawn Han <tosky0514@gmail.com>
2020-07-04 09:42:16 +0900

    Completed 01_getRunCateDistance.js

---
+++ 01_getRunCateDistance.js
@@ -0,0 +1,8 @@
+// TODO: 속력(speed), 시간(time)이 숫자로 주어졌을 때, 이동한 거리를 변수 distance에 할당하여 반환하는
+    // getRunCatDistance 함수를 작성하세요.
+
+function getRunCatDistance(speed, time) {
+    let distance = speed * time;    
+    return distance;
+  }
+  
