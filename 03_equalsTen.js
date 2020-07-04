d15e395609a7aa7201905df9bace66d6edacbcd7
Shawn Han <tosky0514@gmail.com>
2020-07-04 10:33:48 +0900

    Completed 03_equalsTen.js

---
+++ 03_equalsTen.js
@@ -0,0 +1,11 @@
+//Write a function called "equalsTen". ("equalsTen" 함수를 작성하세요.)
+
+//Given a number, "equalsTen" returns whether or not the given number is 10. ("숫자가 주어졌을때, "equalsTen" 함수는 숫자가 10과 같은지 반환합니다.)
+
+function equalsTen(num) {
+    if(num === 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
