1021ede7172de72df822d281b9a8477727d45d85
Shawn Han <tosky0514@gmail.com>
2020-07-04 10:34:03 +0900

    Completed 04_isGreaterThan.js

---
+++ 04_isGreaterThan.js
@@ -0,0 +1,11 @@
+//Write a function called "isGreaterThan". ("isGreaterThan" 함수를 작성하세요.)
+
+//Given 2 numbers, "isGreaterThan" returns whether num2 is greater than num1. (두 숫자가 주어졌을때, "isGreaterThan" 함수는 두번째 숫자(num2)가 첫번째 숫자(num1)보다 큰지 반환해야 합니다.)
+
+function isGreaterThan(num1, num2) {
+    if(num2 > num1){
+        return true;
+    } else {
+        return false;
+    }
+}
