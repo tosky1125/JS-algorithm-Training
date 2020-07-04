5c3dea0bb134d579e33623436a8f01c8a01bbf92
Shawn Han <tosky0514@gmail.com>
2020-07-04 10:33:15 +0900

    Completed 02_declareFunction.js

---
+++ 02_declareFunction.js
@@ -0,0 +1,14 @@
+  // TODO : 첫 번째 인자에 2를 곱하는 함수 multiplyBy2와 비슷하게
+  // 첫 번째 인자를 2로 나누는 함수 divideBy2를 선언하세요.
+
+
+function multiplyBy2(input) {
+    input = input * 2;
+    return input;
+}
+
+function divideBy2(input) {
+    return input / 2;
+}
+
+
